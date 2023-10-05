459709661119309e088424cd963cde471483195a
Benjamín Sánchez <benjamenworks@gmail.com>
2023-10-05 00:08:11 -0300

    Actualizacion V1.2.9.5
    Vista cambiada pero falta el Horario Guardado.

--- Software2/src/App.jsx
+++ Software2/src/App.jsx
@@ -1,11 +1,11 @@
 import { Administrativos } from './components/Administrativos';
 import { Docentes } from './components/Docentes';
 import { Home } from './components/Home';
-import { RegistrarDisponible } from './components/registrarDisponible';
+import { RegistrarDisponible } from './components/Header+registrarDisponible';
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { BuscarProfesor } from './components/buscarProfesor';
-import ProfesorPage from './components/ProfesorPage';
+import { BuscarProfesor } from './components/Header+buscarProfesor';
+import { ProfesorHeader } from './components/Header+ProfesorPage';
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Route path="/Docentes/registrar-horarios" element={<RegistrarDisponible/>} />
         <Route path="/Administrativos" element={<Administrativos />} />
         <Route path="/Administrativos/buscar-profesor" element={<BuscarProfesor />} />
-        <Route path="/Administrativos/buscar-profesor/:id" element={<ProfesorPage />} />
+        <Route path="/Administrativos/buscar-profesor/:id" element={<ProfesorHeader/>} />
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </div>
