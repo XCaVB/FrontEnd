8eb3593d15fb0301646d64d03ed268a6a7e063f6
Javier <j.gavilnorellana@uandresbello.edu>
2023-11-28 17:09:35 -0300

    Actualizacion V1.4.8

--- ProyectoDjango/auditoria/views.py
+++ ProyectoDjango/auditoria/views.py
@@ -6,17 +6,3 @@ from .models import *
 class AuditoriaSeri(viewsets.ModelViewSet):
     serializer_class = AuditoriaSerializer
     queryset = Auditoria.objects.all()
-
-
-
-#TEST DE USUARIO
-
-
-from django.contrib.auth import authenticate
-
-user = authenticate(username="o.salinassilva@uandresbello.edu", password="116220792!")
-
-if user is not None:
-    print('CORRECTO')
-else:
-    print('MAL')
